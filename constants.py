7de6f1863105a2126d87ee4dcbe7d03d46957c94
Martin-Labenne <martinlabenne@gmail.com>
2024-11-16 18:32:26 +0100

    complete data analysis

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
 class KaggleConstants(object):
-    def __init__(self):
-        self.MOBILE_DATASET_HANDLE = 'valakhorasani/mobile-device-usage-and-user-behavior-dataset'
-        self.MOBILE_DATASET_FILE_NAME = 'user_behavior_dataset.csv'
+    MOBILE_DATASET_HANDLE = 'valakhorasani/mobile-device-usage-and-user-behavior-dataset'
+    MOBILE_DATASET_FILE_NAME = 'user_behavior_dataset.csv'

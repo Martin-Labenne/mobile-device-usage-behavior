64645aa4705177e7a79e5b4747b9e800a2f4c0ee
Martin-Labenne <martinlabenne@gmail.com>
2024-11-16 12:00:05 +0100

    setup project

---
+++ constants.py
@@ -0,0 +1,4 @@
+class KaggleConstants(object):
+    def __init__(self):
+        self.MOBILE_DATASET_HANDLE = 'valakhorasani/mobile-device-usage-and-user-behavior-dataset'
+        self.MOBILE_DATASET_FILE_NAME = 'user_behavior_dataset.csv'

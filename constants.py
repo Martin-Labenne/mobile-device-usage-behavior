5475f513aa5b3507c53032682e1ea2178be3a8ee
Martin-Labenne <martinlabenne@gmail.com>
2024-11-18 14:42:36 +0100

    completed preprocessing

--- constants.py
+++ constants.py
@@ -1,3 +1,12 @@
 class KaggleConstants(object):
     MOBILE_DATASET_HANDLE = 'valakhorasani/mobile-device-usage-and-user-behavior-dataset'
     MOBILE_DATASET_FILE_NAME = 'user_behavior_dataset.csv'
+
+class ModelSelectionConstants(object): 
+    MODEL_SELECTION_FOLDER = 'model_selection/'
+    DATA_FOLDER = MODEL_SELECTION_FOLDER + 'data/'
+    MODEL_FOLDER = MODEL_SELECTION_FOLDER + 'models/'
+
+class RandomStateConstants(object): 
+    TRAIN_TEST_SPLIT = 424242
+    QUANTILE_SCALER = 123456
